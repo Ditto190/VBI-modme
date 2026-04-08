908eb9c27375027d7476996e8b660ffdc9671d7d
jiangtao.young <jiangtao.young@qq.com>
2026-04-08 18:21:06 +0800

    remove legacy report text dsl

--- packages/vbi/src/types/reportDSL/index.ts
+++ packages/vbi/src/types/reportDSL/index.ts
@@ -1,4 +1,3 @@
-export type { VBIReportTextDSL, VBIReportTextDSLInput } from './text'
 export type { VBIReportPageDSL, VBIReportPageDSLInput } from './page'
 export type { VBIReportDSL, VBIReportDSLInput } from './report'
 export type { VBIReportSnapshotDSL, VBIReportSnapshotDSLInput } from './snapshot'
