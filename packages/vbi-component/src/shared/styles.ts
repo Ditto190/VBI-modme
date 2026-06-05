f7961f83d8e21fdf41a40f87c9028fc3f3477a2f
tran nhat minh <trannhatminh.dev@gmail.com>
2026-06-05 17:27:13 +0700

    style(vbi-component): add box-sizing

--- packages/vbi-component/src/shared/styles.ts
+++ packages/vbi-component/src/shared/styles.ts
@@ -2,7 +2,14 @@ import { css } from 'lit'
 
 export const defaultStyles = css`
   :host {
+    box-sizing: border-box;
     font-family: var(--vbi-font-family, -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif);
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
   }
 
   :host([hidden]) {
